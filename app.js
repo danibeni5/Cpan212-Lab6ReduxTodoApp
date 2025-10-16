2fe676d23b6f1dca0aa4f812d0a3f808d918f5de
Daniel Stepanenko <daniel.step.ds@gmail.com>
2025-10-15 21:23:59 -0400

    Initial commit: Set up Redux Todo App with basic structure

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ import {Provider} from 'react-redux';
 import {SafeAreaView, StatusBar} from 'react-native';
 import {store} from './src/store';
 import TodoListScreen from './src/screens/TodoListScreen';
-
 const App = () => {
-  return (
-    <Provider store={store}>
-      <SafeAreaView style={{flex: 1}}>
-        <StatusBar barStyle="dark-content" />
-        <TodoListScreen />
-      </SafeAreaView>
-    </Provider>
-  );
+return (
+<Provider store={store}>
+<SafeAreaView style={{flex: 1}}>
+<StatusBar barStyle="dark-content" />
+<TodoListScreen />
+</SafeAreaView>
+</Provider>
+);
 };
-
 export default App;
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-redux|@reduxjs/toolkit|react-native|@react-native|immer|react-native-vector-icons)/)',
+  ],
 };
